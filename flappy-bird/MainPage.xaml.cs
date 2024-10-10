07d2d5202d44ea5f42af8d5e37061dbf8e20e897
paty <patiangelita15@gmail.com>
2024-10-10 08:50:05 -0300

    patyyy

--- flappy-bird/MainPage.xaml.cs
+++ flappy-bird/MainPage.xaml.cs
@@ -3,12 +3,34 @@
 public partial class MainPage : ContentPage
 {
 	
-
+const int gravidade = 40;
+const int tempoEntreFrames =1000;
+bool estaMorto = false;
 	public MainPage()
 	{
 		InitializeComponent();
+		
 	}
 
+
+	void AplicaGravidade()
+	{
+		passaro.TranslationY += gravidade;
+	}
 	
+	async Task Desenhar ()
+	{
+      while (! estaMorto)
+	  {
+		 AplicaGravidade();
+		 await Task.Delay(tempoEntreFrames);
+	  }
+	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenhar();
+    }
+
 }
 
