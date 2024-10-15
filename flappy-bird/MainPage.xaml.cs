f1edf31572740511dd6888e0031471c1963f4bcb
paty <patiangelita15@gmail.com>
2024-10-15 11:26:15 -0300

    paaaty hj

--- flappy-bird/MainPage.xaml.cs
+++ flappy-bird/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace flappy_bird;
+﻿using System.Threading.Channels;
+
+namespace flappy_bird;
 
 public partial class MainPage : ContentPage
 {
@@ -9,6 +11,10 @@ bool estaMorto = true;
 double larguraJanela=0;
 double alturaJanela=0;
 int velocidade=10;
+const int maxTempoPulando = 2;
+int tempoPulando = 0;
+bool estaPulando = false;
+const int forcaPulo = 40;
 	public MainPage()
 	{
 		InitializeComponent();
@@ -20,16 +26,33 @@ int velocidade=10;
 	{
 		passaro.TranslationY += gravidade;
 	}
+	 void AplicaPulo()
+	 {
+		passaro.TranslationY -= forcaPulo;
+		tempoPulando++;
+		if (tempoPulando >= maxTempoPulando)
+		{
+			estaPulando = false;
+			tempoPulando = 0;
+		}
+	 }
 	
 	async Task Desenhar ()
 	{
-      while (! estaMorto)
+      while (! estaMorto) 
 	  {
-
-		 AplicaGravidade();
-
-		 await Task.Delay(tempoEntreFrames);
+		if (estaPulando)
+		AplicaPulo();
+		else
+		AplicaGravidade();
 		 GerenciaCanos();
+		 if (VerificaColisao())
+		 {
+         estaMorto = true;
+		 frameGameOver.IsVisible = true;
+		 break;
+		 }
+		 await Task.Delay(tempoEntreFrames);
 	  }
 	}
    
@@ -64,6 +87,45 @@ int velocidade=10;
 		estaMorto=false;
 		passaro.TranslationY=0;
 	}
+ 
+ bool VerificaColisaoTeto()
+ {
+	var minY = -alturaJanela/2;
+	if (passaro.TranslationY <= minY)
+	return true;
+	else
+	return false;
+ }
+
+ bool VerificaColisaoChao()
+ {
+	var maxY = alturaJanela/2 - imgchao.HeightRequest;
+	if (passaro.TranslationY >= maxY)
+	return true;
+	else
+	return false;
+ }
+ bool VerificaColisao()
+ {
+	if (!estaMorto)
+	{
+   if (VerificaColisaoTeto()|| 
+   VerificaColisaoChao())
+   {
+	return true;
+   }
+	}
+	return false;
+ }
+ void PatyChataDesmais(object s,TappedEventArgs a)
+ {
+	estaPulando=true;
+ }
+
+ 
+ 
+
+
 
 }
 
