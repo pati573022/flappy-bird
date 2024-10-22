3987d1ab32dd5cb74ca1a5174793af5cabd59ed0
paty <patiangelita15@gmail.com>
2024-10-22 10:56:43 -0300

    paaty

--- flappy-bird/MainPage.xaml.cs
+++ flappy-bird/MainPage.xaml.cs
@@ -5,23 +5,23 @@ namespace flappy_bird;
 
 public partial class MainPage : ContentPage
 {
-	
-const int gravidade = 8;
-const int tempoEntreFrames = 25;
-bool estaMorto = true;
-double larguraJanela=0;
-double alturaJanela=0;
-int velocidade=10;
-const int maxTempoPulando = 2;
-int tempoPulando = 0;
-bool estaPulando = false;
-const int forcaPulo = 40;
-const int aberturaMin = 100;
-int score = 0;
+
+	const int gravidade = 8;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = true;
+	double larguraJanela = 0;
+	double alturaJanela = 0;
+	int velocidade = 10;
+	const int maxTempoPulando = 2;
+	int tempoPulando = 0;
+	bool estaPulando = false;
+	const int forcaPulo = 40;
+	const int aberturaMin = 100;
+	int score = 0;
 	public MainPage()
 	{
 		InitializeComponent();
-		
+
 	}
 
 
@@ -29,8 +29,8 @@ int score = 0;
 	{
 		passaro.TranslationY += gravidade;
 	}
-	 void AplicaPulo()
-	 {
+	void AplicaPulo()
+	{
 		passaro.TranslationY -= forcaPulo;
 		tempoPulando++;
 		if (tempoPulando >= maxTempoPulando)
@@ -38,103 +38,118 @@ int score = 0;
 			estaPulando = false;
 			tempoPulando = 0;
 		}
-	 }
-	
-	async Task Desenhar ()
-	{
-      while (! estaMorto) 
-	  {
-		if (estaPulando)
-		AplicaPulo();
-		else
-		AplicaGravidade();
-		 GerenciaCanos();
-		 if (VerificaColisao())
-		 {
-         estaMorto = true;
-		 frameGameOver.IsVisible = true;
-		 break;
-		 }
-		 await Task.Delay(tempoEntreFrames);
-	  }
 	}
-   
-    protected override void OnSizeAllocated(double w, double h)
-    {
-    base.OnSizeAllocated(w, h);
-	larguraJanela=w;
-	alturaJanela=h;
-    }
+
+	async Task Desenhar()
+	{
+		while (!estaMorto)
+		{
+			if (estaPulando)
+				AplicaPulo();
+			else
+				AplicaGravidade();
+			GerenciaCanos();
+			if (VerificaColisao())
+			{
+				estaMorto = true;
+				frameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		larguraJanela = w;
+		alturaJanela = h;
+	}
 
 	void GerenciaCanos()
 	{
-		imgCanocima.TranslationX-= velocidade;
-		imgCanobaixo.TranslationX-= velocidade;
-		if(imgCanobaixo.TranslationX<=-larguraJanela)
+		imgCanocima.TranslationX -= velocidade;
+		imgCanobaixo.TranslationX -= velocidade;
+		if (imgCanobaixo.TranslationX <= -larguraJanela)
 		{
-			imgCanobaixo.TranslationX=0;
-			imgCanocima.TranslationX=0;
-			var alturaMax=-100;
-			var alturaMin=-imgCanobaixo.HeightRequest;
+			imgCanobaixo.TranslationX = 0;
+			imgCanocima.TranslationX = 0;
+			var alturaMax = -100;
+			var alturaMin = -imgCanobaixo.HeightRequest;
 			imgCanocima.TranslationY = Random.Shared.Next((int)alturaMin, (int)alturaMax);
 			imgCanobaixo.TranslationY = imgCanocima.TranslationY + alturaMin + aberturaMin + imgCanobaixo.HeightRequest;
 			score++;
-			labelScore.Text="Canos:" + score.ToString("D3");
+			labelScore.Text = "Canos:" + score.ToString("D3");
 		}
 
 	}
 
-	void OnGameOverClicked(object s,TappedEventArgs a)
+	void OnGameOverClicked(object s, TappedEventArgs a)
 	{
-		frameGameOver.IsVisible=false;
+		frameGameOver.IsVisible = false;
 		Inicializar();
 		Desenhar();
 	}
 
 	void Inicializar()
 	{
-		estaMorto=false;
-		passaro.TranslationY=0;
+		estaMorto = false;
+		passaro.TranslationY = 0;
+		imgCanocima.TranslationX = -larguraJanela;
+		imgCanobaixo.TranslationX = -larguraJanela;
+		passaro.TranslationX = 0;
+		passaro.TranslationY = 0;
+		score = 0;
+		GerenciaCanos();
 	}
- 
- bool VerificaColisaoTeto()
- {
-	var minY = -alturaJanela/2;
-	if (passaro.TranslationY <= minY)
-	return true;
-	else
-	return false;
- }
 
- bool VerificaColisaoChao()
- {
-	var maxY = alturaJanela/2 - imgchao.HeightRequest;
-	if (passaro.TranslationY >= maxY)
-	return true;
-	else
-	return false;
- }
- bool VerificaColisao()
- {
-	if (!estaMorto)
+	bool VerificaColisaoTeto()
 	{
-   if (VerificaColisaoTeto()|| 
-   VerificaColisaoChao())
-   {
-	return true;
-   }
+		var minY = -alturaJanela / 2;
+		if (passaro.TranslationY <= minY)
+			return true;
+		else
+			return false;
 	}
-	return false;
- }
- void PatyChataDesmais(object s,TappedEventArgs a)
- {
-	estaPulando=true;
- }
 
- 
- 
+	bool VerificaColisaoChao()
+	{
+		var maxY = alturaJanela / 2 - imgchao.HeightRequest;
+		if (passaro.TranslationY >= maxY)
+			return true;
+		else
+			return false;
+	}
+	bool VerificaColisao()
+	{
+		if (!estaMorto)
+		{
+			if (VerificaColisaoTeto() ||
+			VerificaColisaoChao() ||
+				VerificaColisaoCanocima())
+				return true;
+			else
+				return false;
+		}
+		void AplicaPulo(object s, TappedEventArgs a)
+		{
+			estaPulando = true;
+		}
+		bool VerificaColisaoCanocima()
+		{
+			var posHpassaro = (larguraJanela / 2) - (passaro.WidthRequest / 2);
+			var posVpassaro = (alturaJanela / 2) - (passaro.HeightRequest / 2) + passaro.TranslationY;
+			if (posHpassaro >= Math.Abs(imgCanocima.TranslationX) - imgCanocima.WidthRequest &&
+			posHpassaro <= Math.Abs(imgCanocima.TranslationX) + imgCanocima.WidthRequest &&
+			posVpassaro <= imgCanocima.HeightRequest + imgCanocima.TranslationY)
 
-
-
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
 }
-
